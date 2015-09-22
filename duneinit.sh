1dfcef5e0bc0e0b124a174dd04aec3e64ffd1da7
David Adams <dladams@bnl.gov>
2015-09-22 07:04:50 -0500

    Add initilization for Darwin (mac Yosemite).

--- duneinit.sh
+++ duneinit.sh
@@ -4,7 +4,14 @@ fi
 source /grid/fermiapp/products/setups.sh
 source /grid/fermiapp/products/larsoft/setup
 source /grid/fermiapp/products/dune/setup_dune.sh
-setup git
+FLAVOR=`ups flavor`
+if echo $FLAVOR | grep ^Linux; then
+  setup git
+else if echo $FLAVOR | grep ^Darwin; then
+  setup getopt v1_1_6
+else
+  echo duneinit.sh: WARNING: Unknown UPS flavor: $FLAVOR
+fi; fi
 setup gitflow
 setup mrb
 export MRB_PROJECT=larsoft
