72fb5651e04046f16bf8546197a4fec11ca24626
David Adams <dladams@bnl.gov>
2015-09-22 08:11:27 -0400

    Changes for Darwin.

--- duneinit.sh
+++ duneinit.sh
@@ -1,9 +1,16 @@
 if [ -n "$LBNE_VERBOSE" ]; then
   echo Setting up PRODUCTS for FNAL
 fi
-source /grid/fermiapp/products/setups.sh
-source /grid/fermiapp/products/larsoft/setup
-source /grid/fermiapp/products/dune/setup_dune.sh
+if [ -r /grid/fermiapp/products ]; then
+  source /grid/fermiapp/products/setups.sh
+  source /grid/fermiapp/products/larsoft/setup
+  source /grid/fermiapp/products/dune/setup_dune.sh
+else if [ -n "$PRODUCTS" -a -r $PRODUCTS ]; then
+  source $PRODUCTS/setups
+else
+  echo duneinit.sh: ERROR: UPS directory not found
+  echo Either PRODUCTS must be defined or /grid/fermiapp/products must be present
+fi; fi
 setup git
 setup gitflow
 setup mrb
